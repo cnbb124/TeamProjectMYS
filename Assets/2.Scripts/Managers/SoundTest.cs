463ff51b91b799723507b1c374c6dcca952bc2cf
Jayeon <cnbb124@gmail.com>
2026-04-23 17:44:52 +0900

    0423 사운드매니저 수정, 테스트씬추가 테스트씬에서 사운드매니저 테스트해봄 우석님이 만든 로비용 BGM추가 다시올림

---
+++ Assets/2.Scripts/Managers/SoundTest.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTest : MonoBehaviour
+{
+	public float sfx3DDelay;
+	public float sfxUiDelay;
+	float sfx3DTimer = 0f;
+	float sfxUiTimer = 0f;
+	SoundManager _soundManager;
+	// Start is called before the first frame update
+	void Start()
+	{
+		_soundManager = SoundManager.Instance;
+		_soundManager.PlayBGM(SOUND_TYPE.BGM_LOBBY);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//타이머0에서 프레임반환시간더하기
+		sfx3DTimer += Time.deltaTime;
+		sfxUiTimer += Time.deltaTime;
+		//타이머가 딜레이시간이 되면
+		if (sfx3DTimer >= sfx3DDelay)
+		{
+			sfx3DTimer = 0;
+			_soundManager.Play3DSFXAtPosition(SOUND_TYPE.SFX_SHOOT, transform.position);
+
+		}
+		if (sfxUiTimer >= sfxUiDelay)
+		{
+			sfxUiTimer = 0;
+			_soundManager.PlayUISFX(SOUND_TYPE.SFX_UI_CLICK);
+		}
+	}
+}
