ba6bc1384ae2cba70cee3d85f74e93f5df25d006
Jayeon <cnbb124@gmail.com>
2026-05-15 11:50:18 +0900

    0515 11:49 자연 플레이어 히트박스설정용 오브젝트추가, 기타 파일수정

--- Assets/2.Scripts/UI/PartData.cs
+++ Assets/2.Scripts/UI/PartData.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 
 // 유니티 우클릭 메뉴에서 쉽게 생성할 수 있게 달아주는 어트리뷰트!
+
+
+
 [CreateAssetMenu(fileName = "New Part Data", menuName = "Gallag/Part Data")]
 public class PartData : ScriptableObject
 {
-    [Header("기본 정보")]
+	[Header("기본 정보")]
     public string partID; // 파츠 고유 ID (ex: "Fighter_01")
     public string partName; // 텍스트에 띄울 이름
     public Sprite partIcon; // 노드 버튼에 들어갈 아이콘 이미지
@@ -16,5 +19,6 @@ public class PartData : ScriptableObject
     public int speed;
     public int price; // 해금 비용 (MRP 등)
 
+    
     // TODO: 갤러그 미연시에 맞게 호감도 스탯이나 특수 기믹 변수들을 여기에 추가.
 }
