79d4f81498e18f7936249548db21f793aa8fe659
Jayeon <cnbb124@gmail.com>
2026-06-20 12:23:23 +0900

    0620 12:23 자연 에러수정

--- Assets/FORGE3D/Sci-Fi Effects/Code/F3DTime.cs
+++ Assets/FORGE3D/Sci-Fi Effects/Code/F3DTime.cs
@@ -65,23 +65,9 @@ namespace FORGE3D
         /// </summary>
         void Awake()
         {
-            if (time != null && time != this)
-            {
-                Destroy(gameObject);
-                return;
-            }
             time = this;
             timers = new List<Timer>();
             removalPending = new List<int>();
-        }
-
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        static void AutoCreate()
-        {
-            if (time != null) return;
-            GameObject go = new GameObject("F3DTime");
-            go.AddComponent<F3DTime>();
-            DontDestroyOnLoad(go);
         }
 
         /// <summary>
