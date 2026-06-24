8f4669eb0b7b32a6ad54e88151e0336aa3c03598
DESKTOP-LKQ38HD\m <michael139k@gmail.com>
2026-06-24 17:54:30 +0900

    260624 17:52 석용 부스터/역추진/쉴드파괴 이펙트 정리 / 부스트 처음실행시 나오는 닷지 부스터는 수정중. - 측면 이동(A/D) RCS 분사 추가 - 역추진을 부스터 누른 시간 기반으로 변경 (연타 방지) - 닷지 시 메인/보조 부스터 fire_3-3 발동 - 쉴드 파괴 이펙트 pulsewave 제거, 순수 파티클로 재구성 - ShieldFXTime 삭제 (미사용), F3DPulsewave 원본 복원

--- Assets/FORGE3D/Sci-Fi Effects/Code/F3DPulsewave.cs
+++ Assets/FORGE3D/Sci-Fi Effects/Code/F3DPulsewave.cs
@@ -53,7 +53,7 @@ namespace FORGE3D
             // Set required flags and set delayed fade flag using timer 
             isEnabled = true;
             isFadeOut = false;
-            timerID = ShieldFXTime.time.AddTimer(FadeOutDelay, OnFadeOut);
+            timerID = F3DTime.time.AddTimer(FadeOutDelay, OnFadeOut);
 
             // Reset default color
             meshRenderer.material.SetColor(tintColorRef, defaultColor);
@@ -66,7 +66,7 @@ namespace FORGE3D
             // Remove timer
             if (timerID >= 0)
             {
-                ShieldFXTime.time.RemoveTimer(timerID);
+                F3DTime.time.RemoveTimer(timerID);
                 timerID = -1;
             }
         }
