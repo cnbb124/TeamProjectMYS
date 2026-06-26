57899c4d54b1a3c67f24df35baf20120657f1e10
DESKTOP-1SE37VB\user <llen0227@gmail.com>
2026-06-26 12:42:39 +0900

    종찬 - Base_Landing 씬 제작 (기체 업그레이드)

--- Assets/2.Scripts/Maps/SunFollow.cs
+++ Assets/2.Scripts/Maps/SunFollow.cs
@@ -5,32 +5,32 @@ public class SunFollow : MonoBehaviour
     public Transform player;
     public Vector3 worldOffset = new Vector3(800f, 400f, 750f);
 
-    // private Vector3 originalScale;
+    private Vector3 originalScale;
 
     void Start()
     {
-       // originalScale = transform.localScale;
+        originalScale = transform.localScale;
     }
 
     void LateUpdate()
     {
         transform.position = player.position + worldOffset;
 
-        //if (Camera.main != null)
-        //{
-            
-        //    Vector3 camToSunDir = (transform.position - Camera.main.transform.position).normalized;
-        //    float cosAngle = Vector3.Dot(Camera.main.transform.forward, camToSunDir);
+        if (Camera.main != null)
+        {
 
-            
-        //    if (cosAngle > 0)
-        //    {
-                
-        //        transform.localScale = originalScale * cosAngle;
-        //    }
+            Vector3 camToSunDir = (transform.position - Camera.main.transform.position).normalized;
+            float cosAngle = Vector3.Dot(Camera.main.transform.forward, camToSunDir);
 
-            
-        //    transform.rotation = Camera.main.transform.rotation;
-        //}
+
+            if (cosAngle > 0)
+            {
+
+                transform.localScale = originalScale * cosAngle;
+            }
+
+
+            transform.rotation = Camera.main.transform.rotation;
+        }
     }
 }
