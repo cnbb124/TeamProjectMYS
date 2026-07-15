126f085c166ac7650b06dc733ff67c5dd0ed598c
Jayeon <cnbb124@gmail.com>
2026-07-15 17:48:11 +0900

    0715 17:34 자연 enumType PROJECTILE_LASER 제거 웨이브 임시 설정

--- Assets/2.Scripts/Managers/LoadingManager.cs
+++ Assets/2.Scripts/Managers/LoadingManager.cs
@@ -32,7 +32,7 @@ public class LoadingManager : MonoBehaviour
     [SerializeField] private float fadeDuration  = 1f;   // 이미지 전환 페이드 시간
     [SerializeField] private string[] tips;
 
-    public static string NextScene = "GameScene";
+    public static SCENE_TYPE NextScene = SCENE_TYPE.MAIN;
 
     private int _currentImageIndex = 0;
 
